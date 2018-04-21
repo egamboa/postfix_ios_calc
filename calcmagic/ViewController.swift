b1e67ce9fedcde2fb14a032aeabff42f66ed2b7e
Esteban Gamboa <estebangam1@gmail.com>
2018-04-20 19:25:52 -0600

    init

--- calcmagic/ViewController.swift
+++ calcmagic/ViewController.swift
@@ -10,16 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var currentValue: UILabel!
+    @IBOutlet weak var operation: UILabel!
+    var isLastActionAOperator = false;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        currentValue.text = ""
+        operation.text = ""
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func calc(_ sender: UIButton) {
+        print('calculating');
+    }
+    
+    @IBAction func reset(_ sender: UIButton) {
+        print('reseting');
+    }
+    
+    @IBAction func isOperator(_ sender: UIButton) {
+        isLastActionAOperator = true;
+        currentValue.text = String(sender.tag)
+    }
+    
+    @IBAction func isNumber(_ sender: UIButton) {
+        if (isLastActionAOperator) {
+            isLastActionAOperator = false
+            operation.text = operation.text! + String(sender.tag)
+        }
+        currentValue.text = currentValue.text! + String(sender.tag)
+    }
+    
 }
 
